553123ec257aead4ddd97aae67b998e61c18b18c
Griswald Brooks <griswald.brooks@gmail.com>
2024-10-07 15:02:33 +0000

    Add test directory

---
+++ test/test_rrt.cpp
@@ -0,0 +1,19 @@
+// C++ Standard Library
+
+// Gtest
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+// Code to test
+#include "rrt.hpp"
+
+
+TEST(bresenham_conversion, overloaded_function_check){
+	// GIVEN two endpoints
+
+	// WHEN the pixels are produced via the two bresenham functions
+
+	// THEN the two vectors should be the same size and be equal to each other
+  EXPECT_FALSE(true);
+}
+
